1247c9c0bcb2b588be999d7bf1cb2cbaabdb5d5a
limaojunio <ecjrcl@gmail.com>
2025-10-26 14:35:09 -0300

    criação de projeto com vite, compronentes: home, navbar e footer, instalação e config do tailwind, estilização e rotas com react router dom

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
