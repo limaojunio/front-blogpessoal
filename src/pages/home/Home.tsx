e63bfa8b1b730eeb339b3bb9387c90f2726ace6b
limaojunio <ecjrcl@gmail.com>
2025-10-31 18:10:37 -0300

    Lista_Postagens_Logica_Styling

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,19 @@
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens";
+
 function Home() {
   return (
     <>
       <div className="bg-indigo-900 flex justify-center">
-        <div className='container grid grid-cols-2 text-white'>
+        <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className='text-5xl font-bold'>
-              Seja Bem Vinde!
-            </h2>
-            <p className='text-xl'>
-              Expresse aqui seus pensamentos e opiniões
-            </p>
+            <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
+            <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
 
             <div className="flex justify-around gap-4">
-              <div className='rounded text-white
-                border-white border-solid border-2 py-2 px-4'>
+              <div
+                className="rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4"
+              >
                 Nova Postagem
               </div>
             </div>
@@ -21,15 +21,17 @@ function Home() {
 
           <div className="flex justify-center ">
             <img
-              src="https://i.imgur.com/fyFr11v.png"
+              src="https://i.imgur.com/fyfri1v.png"
               alt="Imagem Página Home"
-              className='w-2/3'
+              className="w-2/3"
             />
           </div>
         </div>
       </div>
+
+      <ListaPostagens />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
