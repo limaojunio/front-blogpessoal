34444942b3292a88b5cc12f27ccaeaf2e18e41a3
limaojunio <ecjrcl@gmail.com>
2025-10-31 18:30:41 -0300

    Renderizacao_Condicional_Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,11 @@
-import { useContext } from "react";
+import { useContext, type ReactNode } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
   const navigate = useNavigate();
 
-  const { handleLogout } = useContext(AuthContext);
+  const { usuario, handleLogout } = useContext(AuthContext);
 
   function logout() {
     handleLogout();
@@ -13,8 +13,10 @@ function Navbar() {
     navigate("/");
   }
 
-  return (
-    <>
+  let component: ReactNode;
+
+  if (usuario.token !== "") {
+    component = (
       <div
         className="w-full flex justify-center py-4
             			   bg-indigo-900 text-white"
@@ -43,8 +45,10 @@ function Navbar() {
           </div>
         </div>
       </div>
-    </>
-  );
+    );
+  }
+
+  return <>{component}</>;
 }
 
 export default Navbar;
