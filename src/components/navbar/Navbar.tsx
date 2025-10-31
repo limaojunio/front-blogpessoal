7acc276973e4c836f1030b037a78beaf123e42fb
limaojunio <ecjrcl@gmail.com>
2025-10-31 17:53:29 -0300

    Form_Tema_Styling_Logica

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,43 +1,39 @@
 import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
-  const navigate = useNavigate();
 
-  const { handleLogout } = useContext(AuthContext);
+    const navigate = useNavigate();
 
-  function logout() {
-    handleLogout();
-    alert("O Usuário foi desconectado com sucesso!");
-    navigate("/");
-  }
+    const { handleLogout } = useContext(AuthContext)
 
-  return (
-    <>
-      <div
-        className="w-full flex justify-center py-4
-            			   bg-indigo-900 text-white"
-      >
-        <div className="container flex justify-between text-lg mx-8">
-          <Link to="/home" className="text-2xl font-bold">
-            Blog Pessoal
-          </Link>
+    function logout() {
 
-          <div className="flex gap-4">
-            Postagens Temas Cadastrar{" "}
-            <Link to="/temas" className="hover:underline">
-              Temas
-            </Link>{" "}
-            Perfil
-            <Link to="" onClick={logout} className="hover:underline">
-              Sair
-            </Link>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg mx-8">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+
+                    <div className='flex gap-4'>
+                        Postagens
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        Perfil
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Navbar;
+export default Navbar
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,28 +1,30 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blogpessoal.onrender.com",
-});
+    baseURL: 'https://blogpessoal-bdo5.onrender.com'
+})
 
-export const cadastrarUsuario = async (
-  url: string,
-  dados: object,
-  setDados: Function
-) => {
-  const resposta = await api.post(url, dados);
-  setDados(resposta.data);
-};
+export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
 
-export const login = async (url: string, dados: object, setDados: Function) => {
-  const resposta = await api.post(url, dados);
-  setDados(resposta.data);
-};
+export const login = async (url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
 
-export const buscar = async (
-  url: string,
-  setDados: Function,
-  header: object
-) => {
-  const resposta = await api.get(url, header);
-  setDados(resposta.data);
-};
+export const buscar = async (url: string, setDados: Function, header: Object) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
+
+export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
