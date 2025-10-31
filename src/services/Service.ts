634c24ee8a8ddc13e8289da73dc7b0ee2dd4d476
limaojunio <ecjrcl@gmail.com>
2025-10-31 17:33:54 -0300

    Lista_temas_lógica_2

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blogpessoal-bdo5.onrender.com",
+  baseURL: "https://blogpessoal.onrender.com",
 });
 
 export const cadastrarUsuario = async (
@@ -15,5 +15,14 @@ export const cadastrarUsuario = async (
 
 export const login = async (url: string, dados: object, setDados: Function) => {
   const resposta = await api.post(url, dados);
+  setDados(resposta.data);
+};
+
+export const buscar = async (
+  url: string,
+  setDados: Function,
+  header: object
+) => {
+  const resposta = await api.get(url, header);
   setDados(resposta.data);
 };
